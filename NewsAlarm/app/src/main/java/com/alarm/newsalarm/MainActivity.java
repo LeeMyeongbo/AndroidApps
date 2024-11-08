02f25c7f36731302971a68e57390f5ef33a304b4
LeeMyeongbo <myeongbo123@naver.com>
2024-11-08 12:54:52 +0900

    modify variable name & erase useless function/view in MainActivity

--- NewsAlarm/app/src/main/java/com/alarm/newsalarm/MainActivity.java
+++ NewsAlarm/app/src/main/java/com/alarm/newsalarm/MainActivity.java
@@ -3,7 +3,6 @@ package com.alarm.newsalarm;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,10 +10,8 @@ import android.provider.Settings;
 import android.util.Pair;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,7 +25,7 @@ import com.google.android.material.button.MaterialButton;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class MainActivity extends AppCompatActivity {
 
     private AlarmManager alarmManager;
     private ItemTouchHelper helper;
@@ -44,44 +41,28 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
             Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
             startActivity(intent);
         }
-        TextView mTextView =  findViewById(R.id.tvNoAlarm);
-        MaterialButton button = findViewById(R.id.btnAdd);
-        button.setOnClickListener(v -> {
+        MaterialButton btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setClass(this, AlarmSetterActivity.class);
             startActivity(intent);
         });
 
         ArrayList<Pair<String, Boolean>> dataset = new ArrayList<>();
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) {
             dataset.add(new Pair<>("0" + i + ":00", i % 2 == 0));
+        }
 
         AlarmlistAdapter adapter = new AlarmlistAdapter(dataset, listenerImpl);
-        RecyclerView alarm_listview = findViewById(R.id.alarmList);
+        RecyclerView lvAlarm = findViewById(R.id.alarmList);
 
-        alarm_listview.setLayoutManager(new LinearLayoutManager(this));
-        alarm_listview.setAdapter(adapter);
+        lvAlarm.setLayoutManager(new LinearLayoutManager(this));
+        lvAlarm.setAdapter(adapter);
 
         helper = new ItemTouchHelper(new ItemTouchHelperCallback(adapter));
-        helper.attachToRecyclerView(alarm_listview);
-
-        if (dataset.isEmpty()) {
-            mTextView.setVisibility(View.VISIBLE);
-            alarm_listview.setVisibility(View.GONE);
-        }
+        helper.attachToRecyclerView(lvAlarm);
     }
 
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        c.set(Calendar.MINUTE, minute);
-        c.set(Calendar.SECOND, 0);
-
-        startAlarm(c);
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
     private void startAlarm(Calendar c) {
         Intent intent = new Intent(this, AlertReceiver.class);
         intent.putExtra("key", "alarm");
