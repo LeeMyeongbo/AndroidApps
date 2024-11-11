e1ad774c088e779074c6b2b5882450847ac3b116
LeeMyeongbo <myeongbo123@naver.com>
2024-11-11 23:22:13 +0900

    modify MainActivity to finish when click backKey twice & add some logs

--- NewsAlarm/app/src/main/java/com/alarm/newsalarm/MainActivity.java
+++ NewsAlarm/app/src/main/java/com/alarm/newsalarm/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.util.Pair;
+import android.view.KeyEvent;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -23,12 +25,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CLASS_NAME = "MainActivity";
     private RecyclerView lvAlarmList;
     private AlarmManager alarmManager;
     private ItemTouchHelper helper;
+    private long backKeyReleasedTime = -1;
+    private long backKeyPressedTime = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(CLASS_NAME, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -92,5 +98,67 @@ public class MainActivity extends AppCompatActivity {
         /* TO DO : start SetterActivity with alarm data located in the 'position' */
         Toast.makeText(this, "clicked : " + (position + 1) + "번 째", Toast.LENGTH_SHORT).show();
         executeAlarmSetterActivity();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(CLASS_NAME, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(CLASS_NAME, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(CLASS_NAME, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(CLASS_NAME, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(CLASS_NAME, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        backKeyPressedTime = System.currentTimeMillis();
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            long curBackKeyReleased = System.currentTimeMillis();
+            if (isBackKeyPressedOverTwoSeconds(curBackKeyReleased)) {
+                return true;
+            }
+            if (isBackKeyReleasedTwoTimesWithinTwoSeconds(curBackKeyReleased)) {
+                finish();
+                return true;
+            }
+            Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르시면 종료합니다.", Toast.LENGTH_SHORT).show();
+            backKeyReleasedTime = curBackKeyReleased;
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private boolean isBackKeyPressedOverTwoSeconds(long curBackKeyReleased) {
+        return curBackKeyReleased - backKeyPressedTime > 2000;
+    }
+
+    private boolean isBackKeyReleasedTwoTimesWithinTwoSeconds(long curBackKeyReleased) {
+        return curBackKeyReleased - backKeyReleasedTime < 2000;
     }
 }
