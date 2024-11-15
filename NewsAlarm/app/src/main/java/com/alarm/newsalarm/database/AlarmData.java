8ae523caf39a28362956a01916c0c8c493ac6b0b
LeeMyeongbo <myeongbo123@naver.com>
2024-11-15 22:54:23 +0900

    make AlarmData class implement Parcelable

--- NewsAlarm/app/src/main/java/com/alarm/newsalarm/database/AlarmData.java
+++ NewsAlarm/app/src/main/java/com/alarm/newsalarm/database/AlarmData.java
@@ -1,11 +1,15 @@
 package com.alarm.newsalarm.database;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "AlarmDataTable")
-public class AlarmData {
+public class AlarmData implements Parcelable {
 
     @PrimaryKey
     private long id;
@@ -33,6 +37,31 @@ public class AlarmData {
 
     @ColumnInfo(name = "Active")
     private boolean isActive = true;
+
+    public static final Creator<AlarmData> CREATOR = new Creator<>() {
+
+        @Override
+        public AlarmData createFromParcel(Parcel source) {
+            return new AlarmData(source);
+        }
+
+        @Override
+        public AlarmData[] newArray(int size) {
+            return new AlarmData[size];
+        }
+    };
+
+    protected AlarmData(Parcel in) {
+        id = in.readLong();
+        alarmName = in.readString();
+        alarmTopic = in.readString();
+        volumeSize = in.readFloat();
+        vibIntensity = in.readInt();
+        specificDateInMillis = in.readLong();
+        periodicWeekBit = in.readByte();
+        periodicTimeInMin = in.readInt();
+        isActive = in.readBoolean();
+    }
 
     public AlarmData(
         long id, String alarmName, String alarmTopic, float volumeSize, int vibIntensity
@@ -114,5 +143,23 @@ public class AlarmData {
 
     public boolean isActive() {
         return isActive;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(alarmName);
+        dest.writeString(alarmTopic);
+        dest.writeFloat(volumeSize);
+        dest.writeInt(vibIntensity);
+        dest.writeLong(specificDateInMillis);
+        dest.writeByte(periodicWeekBit);
+        dest.writeInt(periodicTimeInMin);
+        dest.writeBoolean(isActive);
     }
 }
