ce5df998d8e6478e913a63e6d5a96b6c72aeb448
LeeMyeongbo <myeongbo123@naver.com>
2024-12-16 02:46:49 +0900

    enable to turn off alarm by dragging the button out of boundary

--- NewsAlarm/app/src/main/java/com/alarm/newsalarm/AlarmNotifierActivity.java
+++ NewsAlarm/app/src/main/java/com/alarm/newsalarm/AlarmNotifierActivity.java
@@ -1,7 +1,11 @@
 package com.alarm.newsalarm;
 
+import android.annotation.SuppressLint;
 import android.app.KeyguardManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 
 import com.alarm.newsalarm.alarmmanager.WakeLockUtil;
@@ -11,7 +15,12 @@ import com.google.android.material.button.MaterialButton;
 
 public class AlarmNotifierActivity extends BaseActivity {
 
+    private static final int FINISH_DIST = 100;
+
+    private MaterialButton btnReleaseAlarm;
     private NewsNotifier notifier;
+    private float finishDistInPx;
+    private float radius, centerX, centerY, dx, dy;
 
     public AlarmNotifierActivity() {
         super("AlarmNotifierActivity");
@@ -34,13 +43,51 @@ public class AlarmNotifierActivity extends BaseActivity {
     }
 
     private void init() {
-        MaterialButton btnReleaseAlarm = findViewById(R.id.btnReleaseAlarm);
-        btnReleaseAlarm.setOnClickListener(v -> finish());
+        initBtnRelease();
 
         setShowWhenLocked(true);
         setTurnScreenOn(true);
         KeyguardManager manager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
         manager.requestDismissKeyguard(this, null);
+    }
+
+    private void initBtnRelease() {
+        btnReleaseAlarm = findViewById(R.id.btnReleaseAlarm);
+        btnReleaseAlarm.post(() -> {
+            radius = btnReleaseAlarm.getWidth() / 2f;
+            centerX = btnReleaseAlarm.getX() + radius;
+            centerY = btnReleaseAlarm.getY() + radius;
+            finishDistInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, FINISH_DIST,
+                getResources().getDisplayMetrics());
+            Log.i(CLASS_NAME, "initBtnRelease$finishDistInPx : " + finishDistInPx);
+            setBtnReleaseListener();
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setBtnReleaseListener() {
+        btnReleaseAlarm.setOnTouchListener((view, event) -> {
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN -> {
+                    dx = view.getX() - event.getRawX();
+                    dy = view.getY() - event.getRawY();
+                }
+                case MotionEvent.ACTION_MOVE -> {
+                    float x = event.getRawX() + dx;
+                    float y = event.getRawY() + dy;
+                    view.animate().x(x).y(y).setDuration(0).start();
+                    double distance = Math.pow((x + radius - centerX) * (x + radius - centerX)
+                        + (y + radius - centerY) * (y + radius - centerY), 0.5);
+                    if (distance >= finishDistInPx) {
+                        finish();
+                    }
+                }
+                case MotionEvent.ACTION_UP -> {
+                    view.animate().x(centerX - radius).y(centerY - radius).setDuration(0).start();
+                }
+            }
+            return true;
+        });
     }
 
     @Override
